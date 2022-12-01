2ec54432ec51a28bdd7b8611f1da5044247ceeb6
Peitao Ting <peitao@humcapital.com>
2022-12-01 00:14:29 -0800

    Make it top N instead of top 3

--- day01.py
+++ day01.py
@@ -1,20 +1,20 @@
+N = 3
 
 def main():
     with open('input01.txt', 'r') as f:
         res = 0
-        top3 = [0, 0, 0]
+        top = [0] * N
 
         for line in f:
-            line = line.strip()
+            line = line.rstrip()
             if not line:
-                top3.sort()
-                top3[0] = max(top3[0], res)
+                top.sort()
+                top[0] = max(top[0], res)
                 res = 0
             else:
                 res += int(line)
 
-        # print(f"{top3}")
-        print(top3[0] + top3[1] + top3[2])
+        print(sum(top))
 
 
 if __name__ == "__main__":
