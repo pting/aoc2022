cf1bb7b542c2c7b3d7e7c7aa72ef97ea295f7c29
Peitao Ting <peitao@humcapital.com>
2022-12-01 12:43:26 -0800

    make compile.sh nicer

--- compile.sh
+++ compile.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Usage: compile.sh <myprog> (without .c or .py)"
 python -m nuitka $1.py
 
-gcc -O3 $1.c -o $1
+if [ -e $1.c ]
+then
+    echo "gcc -O3 $1.c -o $1"
+    gcc -O3 $1.c -o $1
+fi
 
 echo "Run: ./$1.bin and ./$1"
