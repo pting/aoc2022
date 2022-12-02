8e430f9bd1f04db56b09570c285c03a747f7b281
Peitao Ting <peitao@humcapital.com>
2022-12-01 21:38:22 -0800

    Day 2

---
+++ day02.py
@@ -0,0 +1,77 @@
+import os
+
+N = 3
+INPUTFILE=os.environ.get("AOC_INPUT", "input02.txt")
+
+def main():
+    with open(INPUTFILE, 'r') as f:
+        score1 = 0
+        score2 = 0
+        round = 0
+
+        for line in f:
+            line = line.strip()
+            line = line.split(" ")
+            # print(f"{line}")
+            round = 0
+
+            # Part 1
+            if line[1] == 'X':
+                round += 1
+            elif line[1] == 'Y':
+                round += 2
+            elif line[1] == 'Z':
+                round += 3
+            # Draw
+            if (line[0] == 'A' and line[1] == 'X') or (line[0] == 'B' and line[1] == 'Y') or (line[0] == 'C' and line[1] == 'Z'):
+                round += 3
+                # print("Draw")
+            # Win
+            elif (line[0] == 'A' and line[1] == 'Y') or (line[0] == 'B' and line[1] == 'Z') or (line[0] == 'C' and line[1] == 'X'):
+                round += 6
+                # print("Win")
+
+            score1 += round
+
+            round = 0
+            # Part 2 - X:lose, Y:draw, Z:Win
+            if line[1] == 'X':
+                if line[0] == 'A':
+                    # pick Z
+                    round += 3
+                elif line[0] == 'B':
+                    # pick X
+                    round += 1
+                elif line[0] == 'C':
+                    # pick Y
+                    round += 2
+            elif line[1] == 'Y':
+                if line[0] == 'A':
+                    # pick X
+                    round += 1
+                elif line[0] == 'B':
+                    # pick Y
+                    round += 2
+                elif line[0] == 'C':
+                    # pick Z
+                    round += 3
+                round += 3
+            elif line[1] == 'Z':
+                if line[0] == 'A':
+                    # pick Y
+                    round += 2
+                elif line[0] == 'B':
+                    # pick Z
+                    round += 3
+                elif line[0] == 'C':
+                    # pick X
+                    round += 1
+                round += 6
+
+            score2 += round
+
+        print(f"Part 1: {score1}, Part 2: {score2}")
+
+
+if __name__ == "__main__":
+    main()
