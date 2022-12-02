78cf3802c83a3bf00feaf723ab47d5c5ebdab50f
Peitao Ting <peitao@humcapital.com>
2022-12-01 22:03:17 -0800

    Use lookups instead

--- day02.py
+++ day02.py
@@ -1,7 +1,48 @@
 import os
 
-N = 3
 INPUTFILE=os.environ.get("AOC_INPUT", "input02.txt")
+
+myselect = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3
+}
+
+part1 = {
+    "A": {
+        "X": 3,
+        "Y": 6,
+        "Z": 0
+    },
+    "B": {
+        "Y": 3,
+        "Z": 6,
+        "X": 0
+    },
+    "C": {
+        "Z": 3,
+        "X": 6,
+        "Y": 0
+    }
+}
+
+part2 = {
+    "A": {
+        "X": (0, "Z"),
+        "Y": (3, "X"),
+        "Z": (6, "Y")
+    },
+    "B": {
+        "X": (0, "X"),
+        "Y": (3, "Y"),
+        "Z": (6, "Z")
+    },
+    "C": {
+        "X": (0, "Y"),
+        "Y": (3, "Z"),
+        "Z": (6, "X")
+    }
+}
 
 def main():
     with open(INPUTFILE, 'r') as f:
@@ -16,59 +57,62 @@ def main():
             round = 0
 
             # Part 1
-            if line[1] == 'X':
-                round += 1
-            elif line[1] == 'Y':
-                round += 2
-            elif line[1] == 'Z':
-                round += 3
-            # Draw
-            if (line[0] == 'A' and line[1] == 'X') or (line[0] == 'B' and line[1] == 'Y') or (line[0] == 'C' and line[1] == 'Z'):
-                round += 3
-                # print("Draw")
-            # Win
-            elif (line[0] == 'A' and line[1] == 'Y') or (line[0] == 'B' and line[1] == 'Z') or (line[0] == 'C' and line[1] == 'X'):
-                round += 6
-                # print("Win")
+            # if line[1] == 'X':
+            #     round += 1
+            # elif line[1] == 'Y':
+            #     round += 2
+            # elif line[1] == 'Z':
+            #     round += 3
+            # # Draw
+            # if (line[0] == 'A' and line[1] == 'X') or (line[0] == 'B' and line[1] == 'Y') or (line[0] == 'C' and line[1] == 'Z'):
+            #     round += 3
+            #     # print("Draw")
+            # # Win
+            # elif (line[0] == 'A' and line[1] == 'Y') or (line[0] == 'B' and line[1] == 'Z') or (line[0] == 'C' and line[1] == 'X'):
+            #     round += 6
+            #     # print("Win")
 
+            round = myselect[line[1]]
+            round += part1[line[0]][line[1]]
             score1 += round
 
             round = 0
             # Part 2 - X:lose, Y:draw, Z:Win
-            if line[1] == 'X':
-                if line[0] == 'A':
-                    # pick Z
-                    round += 3
-                elif line[0] == 'B':
-                    # pick X
-                    round += 1
-                elif line[0] == 'C':
-                    # pick Y
-                    round += 2
-            elif line[1] == 'Y':
-                if line[0] == 'A':
-                    # pick X
-                    round += 1
-                elif line[0] == 'B':
-                    # pick Y
-                    round += 2
-                elif line[0] == 'C':
-                    # pick Z
-                    round += 3
-                round += 3
-            elif line[1] == 'Z':
-                if line[0] == 'A':
-                    # pick Y
-                    round += 2
-                elif line[0] == 'B':
-                    # pick Z
-                    round += 3
-                elif line[0] == 'C':
-                    # pick X
-                    round += 1
-                round += 6
+            # if line[1] == 'X':
+            #     if line[0] == 'A':
+            #         # pick Z
+            #         round += 3
+            #     elif line[0] == 'B':
+            #         # pick X
+            #         round += 1
+            #     elif line[0] == 'C':
+            #         # pick Y
+            #         round += 2
+            # elif line[1] == 'Y':
+            #     if line[0] == 'A':
+            #         # pick X
+            #         round += 1
+            #     elif line[0] == 'B':
+            #         # pick Y
+            #         round += 2
+            #     elif line[0] == 'C':
+            #         # pick Z
+            #         round += 3
+            #     round += 3
+            # elif line[1] == 'Z':
+            #     if line[0] == 'A':
+            #         # pick Y
+            #         round += 2
+            #     elif line[0] == 'B':
+            #         # pick Z
+            #         round += 3
+            #     elif line[0] == 'C':
+            #         # pick X
+            #         round += 1
+            #     round += 6
 
-            score2 += round
+            res = part2[line[0]][line[1]]
+            score2 += (res[0] + myselect[res[1]])
 
         print(f"Part 1: {score1}, Part 2: {score2}")
 
