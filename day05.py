e8c90a6adb0055f2e6f8dd8c9365bb156d0c6016
Peitao Ting <peitao@humcapital.com>
2022-12-05 09:11:41 -0800

    General refactor and cleanup

--- day05.py
+++ day05.py
@@ -2,80 +2,57 @@ import os
 import json
 import re
 
-def parseline(line):
+def parsestack(line):
     n = 4
     return [line[i:i+n] for i in range(0, len(line), n)]
 
 
 with open(os.environ.get("AOC_INPUT", "input05.txt"), "r") as f:
     INPUT = f.read().split("\n")
-    PARSEDINPUT = []
+    STACKINPUT = []
     MOVEMENT = []
     for line in INPUT:
         if line:
             if "[" in line:
-                PARSEDINPUT.append(parseline(line))
+                STACKINPUT.append(parsestack(line))
             elif "move" in line:
-                MOVEMENT.append(line)
-
-
-def getstacks():
-    res = []
-    for line in PARSEDINPUT:
+                move = re.match("move (\d+) from (\d+) to (\d+)", line)
+                moveints = []
+                for item in move.groups():
+                    moveints.append(int(item))
+                MOVEMENT.append(moveints)
+    STACK1 = []
+    STACK2 = []
+    # Parse in reverse order to set up queues
+    for line in STACKINPUT[::-1]:
         ns = len(line)
-        while len(res) < ns:
-            res.append([])
+        while len(STACK1) < ns:
+            STACK1.append([])
+            STACK2.append([])
         for i, stack in enumerate(line):
-            id = re.findall("\[[a-zA-Z]+\]", stack)
+            id = re.match("\[[a-zA-Z]+\]", stack)
             if id:
-                res[i].append(id)
-    return res
-
+                STACK1[i].append(id)
+                STACK2[i].append(id)
 
 
 def part1():
-    stacks = getstacks()
-    # print(f">>> {stacks}")
-    for line in MOVEMENT:
-        move = re.findall("move (\d+) from (\d+) to (\d+)", line)
-        moveint = []
-        for item in move[0]:
-            moveint.append(int(item))
-        # print(moveint)
+    for moveint in MOVEMENT:
         for i in range(moveint[0]):
-            id = stacks[moveint[1]-1].pop(0)
-            # print(f"moving {i}: {id} -----{stacks}")
-            stacks[moveint[2]-1].insert(0, id)
-            # print(stacks)
-    res = ""
-    for row in stacks:
-        res += row[0][0][1]
-    return res
+            id = STACK1[moveint[1]-1].pop()
+            STACK1[moveint[2]-1].append(id)
+    return "".join([row[-1][0][1] for row in STACK1])
 
 
 def part2():
-    stacks = getstacks()
-    # print(f">>> {stacks}")
-    for line in MOVEMENT:
-        move = re.findall("move (\d+) from (\d+) to (\d+)", line)
-        moveint = []
-        for item in move[0]:
-            moveint.append(int(item))
-        # print(moveint)
+    for moveint in MOVEMENT:
         m = []
         for i in range(moveint[0]):
-            id = stacks[moveint[1]-1].pop(0)
-            # print(f"moving {i}: {id} -----{stacks}")
+            id = STACK2[moveint[1]-1].pop()
             m.append(id)
-        
         for id in m[::-1]:
-            stacks[moveint[2]-1].insert(0, id)
-            # print(stacks)
-        
-    res = ""
-    for row in stacks:
-        res += row[0][0][1]
-    return res
+            STACK2[moveint[2]-1].append(id)
+    return "".join([row[-1][0][1] for row in STACK2])
 
 
 def main():
