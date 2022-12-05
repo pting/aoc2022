ba4ed30565580792a3a3a8ca66945203039ce9d4
Peitao Ting <peitao@humcapital.com>
2022-12-05 13:12:20 -0800

    more abbreviated code and black formatting

--- day05.py
+++ day05.py
@@ -2,9 +2,10 @@ import os
 import json
 import re
 
+
 def parsestack(line):
     n = 4
-    return [line[i:i+n] for i in range(0, len(line), n)]
+    return [line[i : i + n] for i in range(0, len(line), n)]
 
 
 with open(os.environ.get("AOC_INPUT", "input05.txt"), "r") as f:
@@ -14,8 +15,14 @@ with open(os.environ.get("AOC_INPUT", "input05.txt"), "r") as f:
     for line in INPUT:
         if line:
             if line.startswith("m"):
-                move = [int(x) for x in re.match("move (\d+) from (\d+) to (\d+)", line).groups()]
-                MOVEMENT.append(move)
+                MOVEMENT.append(
+                    [
+                        int(x)
+                        for x in re.match(
+                            "move (\d+) from (\d+) to (\d+)", line
+                        ).groups()
+                    ]
+                )
             elif "[" in line:
                 STACKINPUT.append(parsestack(line))
     # One for each part
@@ -23,8 +30,7 @@ with open(os.environ.get("AOC_INPUT", "input05.txt"), "r") as f:
     STACK2 = []
     # Parse in reverse order to set up queues
     for line in STACKINPUT[::-1]:
-        ns = len(line)
-        while len(STACK1) < ns:
+        while len(STACK1) < len(line):
             STACK1.append([])
             STACK2.append([])
         for i, stack in enumerate(line):
@@ -37,8 +43,7 @@ with open(os.environ.get("AOC_INPUT", "input05.txt"), "r") as f:
 def part1():
     for moveint in MOVEMENT:
         for _ in range(moveint[0]):
-            id = STACK1[moveint[1]-1].pop()
-            STACK1[moveint[2]-1].append(id)
+            STACK1[moveint[2] - 1].append(STACK1[moveint[1] - 1].pop())
     return "".join([row[-1][0][1] for row in STACK1])
 
 
@@ -46,10 +51,9 @@ def part2():
     for moveint in MOVEMENT:
         m = []
         for _ in range(moveint[0]):
-            id = STACK2[moveint[1]-1].pop()
-            m.append(id)
+            m.append(STACK2[moveint[1] - 1].pop())
         for id in m[::-1]:
-            STACK2[moveint[2]-1].append(id)
+            STACK2[moveint[2] - 1].append(id)
     return "".join([row[-1][0][1] for row in STACK2])
 
 
