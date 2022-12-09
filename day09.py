7404d23c9974e4ba5cef978581bcc26f827816ea
Peitao Ting <peitao@humcapital.com>
2022-12-09 00:16:12 -0800

    Cleanup and black

--- day09.py
+++ day09.py
@@ -1,45 +1,41 @@
 import os
 import json
 
-def pr(matrix):
-    for r in matrix:
-        print(r)
-
 m = set([(0, 0)])
 
-def isfar(h, t):
-    return (abs(h[0] - t[0]) > 1 or abs(h[1] - t[1]) > 1)
+
+def helper(T):
+    for line in INPUT:
+        if not line:
+            continue
+
+        move = line.split(" ")
+        dir = move[0]
+        n = int(move[1])
+        while n:
+            if dir == "L":
+                T[0][0] -= 1
+            elif dir == "R":
+                T[0][0] += 1
+            elif dir == "U":
+                T[0][1] += 1
+            elif dir == "D":
+                T[0][1] -= 1
+            n -= 1
+            movenext(T)
+
+    return len(m)
 
 
-def movehead(T, dir, n):
-    while n:
-        # print(f"movehead: {T} -> {dir}, {n}")
-        if dir == "L":
-            T[0][0] -= 1
-        elif dir == "R":
-            T[0][0] += 1
-        elif dir == "U":
-            T[0][1] += 1
-        elif dir == "D":
-            T[0][1] -= 1
-        n -= 1
-        # print(f"--> H: {H}")
-        
-        movetail(T)
-        
-
-def movetail(tlist):
-    # print(tlist)
+def movenext(tlist):
     if len(tlist) == 1:
-        newloc = tuple(tlist[0])
-        m.add(newloc)
-        # print(f"End: {m}")
+        m.add(tuple(tlist[0]))
         return
 
     h = tlist[0]
     t = tlist[1]
     moved = False
-    if isfar(h, t):
+    if abs(h[0] - t[0]) > 1 or abs(h[1] - t[1]) > 1:
         if h[0] - t[0] > 0:
             t[0] += 1
             moved = True
@@ -52,9 +48,8 @@ def movetail(tlist):
         elif h[1] - t[1] < 0:
             t[1] -= 1
             moved = True
-            # print(f"--> {T}, tot: {tot1}")
     if moved:
-        movetail(tlist[1:])
+        movenext(tlist[1:])
 
 
 with open(os.environ.get("AOC_INPUT", "input09.txt"), "r") as f:
@@ -63,30 +58,15 @@ with open(os.environ.get("AOC_INPUT", "input09.txt"), "r") as f:
 
 def part1():
     T = [[0, 0], [0, 0]]
-    for line in INPUT:
-        if not line:
-            continue
-
-        move = line.split(" ")
-        movehead(T, move[0], int(move[1]))
-    return len(m)
+    return helper(T)
 
 
 def part2():
     T = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
     global m
-    m = set([(0,0)])
-    # print(f"Begin: {T}")
-    for line in INPUT:
-        if not line:
-            continue
+    m = set([(0, 0)])
+    return helper(T)
 
-        move = line.split(" ")
-        movehead(T, move[0], int(move[1]))
-    # print(f"End: {T}")
-    return len(m)
-
- 
 
 def main():
     ret = {}
