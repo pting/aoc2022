844e973a9678652797c576a45d6b91ac29a32e0f
Peitao Ting <peitao@humcapital.com>
2022-12-18 02:39:42 -0800

    Fixed solution

--- day18/day18.py
+++ day18/day18.py
@@ -2,7 +2,7 @@ import os
 import sys
 import json
 import re
-import sys
+
 from collections import deque
 
 sys.setrecursionlimit(1000000)
@@ -19,6 +19,7 @@ def prd(d):
         print(f"{k}: {v}")
 
 M = set()
+bound = [0,0,0]
 path = os.path.dirname(os.path.abspath(__file__))
 inputname = sys.argv[1] if len(sys.argv) > 1 else os.path.join(path, "input.txt")
 with open(os.environ.get("AOC_INPUT", inputname), "r") as f:
@@ -26,46 +27,11 @@ with open(os.environ.get("AOC_INPUT", inputname), "r") as f:
     for l in lines:
         if l:
             cube = tuple(map(int, l.split(",")))
+            bound[0] = max(bound[0], cube[0])
+            bound[1] = max(bound[1], cube[1])
+            bound[2] = max(bound[2], cube[2])
             M.add(cube)
 
-# pr(M)
-
-def isnext(c1, c2):
-    return abs(c1[0] - c2[0]) + abs(c1[1] - c2[1]) + abs(c1[2] - c2[2]) == 1
-
-
-GR = {}
-
-for t in M:
-    GR[t] = []
-    for c in M:
-        if t != c:
-            if isnext(t, c):
-                GR[t].append(c)
-
-# pr(M)
-# prd(GR)
-
-visited = set()
-connected = []
-
-def traverse(k, conn):
-    if k in visited:
-        return None
-    visited.add(k)
-    conn.add(k)
-    for t in GR[k]:
-        traverse(t, conn)
-    
-
-
-for k in GR:
-    conn = set()
-    traverse(k, conn)
-    if conn:
-        connected.append(conn)
-
-# pr(connected)
 
 alldirs = [
     [-1, 0, 0],
@@ -84,6 +50,16 @@ def getallnextair(k):
             ret.append(loc)
     return ret
 
+
+def getallnextlava(k):
+    ret = []
+    for dir in alldirs:
+        loc = tuple((k[0] + dir[0], k[1] + dir[1], k[2] + dir[2]))
+        if loc in M:
+            ret.append(loc)
+    return ret
+
+
 outside = set()
 
 def bfs(s):
@@ -95,51 +71,28 @@ def bfs(s):
             continue
         outside.add(p)
         for n in getallnextair(p):
-            if n[0] < 101 and n[1] < 101 and n[2] < 101 and n[0] >= 0 and n[1] >= 0 and n[2] >= 0 and n not in outside:
+            if n[0] <= bound[0] + 1 and n[1] <= bound[1] + 1 and n[2] <= bound[2] + 1 and n[0] >= -1 and n[1] >= -1 and n[2] >= -1 and n not in outside:
                 q.append(n)
 
-bfs(tuple((0,0,0)))
+
+# outside = set()
+bfs(tuple(bound))
 # print("Done with outside.")
 # print(len(outside))
 
-# outside = set()
-# bfs(tuple((0, 100, 100)))
-# print("Done with outside.")
-# print(len(outside))
-
-# outside = set()
-# bfs(tuple((0, 0, 100)))
-# print("Done with outside.")
-# print(len(outside))
-
-# outside = set()
-# bfs(tuple((0, 100, 0)))
-# print("Done with outside.")
-# print(len(outside))
-
-# outside = set()
-# bfs(tuple((100, 100, 100)))
-# print("Done with outside.")
-# print(len(outside))
 
 def part1():
     total = 0
-    for group in connected:
-        gtot = 6 * len(group)
-        for t in group:
-            gtot -= len(GR[t])
-        total += gtot
+    for x in M:
+        total += 6 - len(getallnextlava(x))
 
     return(total)
 
 
 def part2():
     total = 0
-    for group in connected:
-        gtot = 6 * len(group)
-        for t in group:
-            gtot -= len(GR[t])
-        total += gtot
+    for x in M:
+        total += 6 - len(getallnextlava(x))
 
     for k in M:
         for a in getallnextair(k):
